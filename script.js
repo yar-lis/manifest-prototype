f81af17ee2afcf5c41e8bb40427bacf0c212d3c3
yar-lis <yar11lisach@gmail.com>
2025-06-05 15:18:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const modal = document.getElementById("modal");
+const openModal = document.getElementById("openModal");
+const closeModal = document.getElementById("closeModal");
+
+openModal.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+});
