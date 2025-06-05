2303468aad05dec8dfbbc0447721e7db8360d7d6
yar-lis <yar11lisach@gmail.com>
2025-06-05 18:49:02 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Сайт MANIфест загружен');
+
+const modal = document.getElementById("modal");
+const openModal = document.getElementById("openModal");
+const closeModal = document.getElementById("closeModal");
+
+openModal.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+});
